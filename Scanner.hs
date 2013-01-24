ee1f701040386fa63a3b59d62309c6346b57d0c5
Murph Murphy <omurcada@gmail.com>
2013-01-24 01:30:24 -0700

    * The scanner base stuff should completely work. Testing and modification will likely have to be done when the FSA's are done. * closes #17, closes #11, closes #12, closes #13, closes #14

--- Scanner.hs
+++ Scanner.hs
@@ -1,19 +1,28 @@
+-- Authored by: Ryan "Murph" Murphy
+-- Date: 1/24/2013
+--
+-- This file contains all the top level methods required for the Scanner to
+-- function.
+
 module Scanner
-(
-    getToken,
-    skipWhitespace
-) where
+    (
+    , getToken
+    , skipWhitespace
+    , getLexeme
+    , getLineNumber
+    , getColumnNumber
+    ) where
+
+import Data.Char (isDigit, isLetter, isControl, isSpace)
 
 -- | Starts with the input string (assuming at first character of next lexeme),
 -- an empty lexeme, and the current line and column numbers.
 -- | Returns all the same things, expecting the reciever to extract the final
 -- token
-import Data.Char (isDigit, isLetter)
-
 getToken :: (String, String, Int, Int) -> (String, String, Int, Int)
 getToken (source, lexeme, columnNumber, lineNumber)
-    | nextChar == ' '
-        = skipWhitespace (source, lexeme, newCol = columnNumber + 1, lineNumber)
+    | isSpace nextChar
+        = skipWhitespace (nextChar : source, lexeme, columnNumber, lineNumber)
     | nextChar == '('
         = lParenFSA (nextChar : source, lexeme, columnNumber, lineNumber)
     | nextChar == ')'
@@ -26,11 +35,29 @@ getToken (source, lexeme, columnNumber, lineNumber)
         = letterFSA (nextChar : source, lexeme, columnNumber, lineNumber)
     | isDigit nextChar
         = digitFSA (nextChar : source, lexeme, columnNumber, lineNumber)
-    where nextChar = head source  -- get the next character
+  where
+    nextChar = head source  -- get the next character
 
-
--- | skipWhitespace expects to recieve parameters that have already skipped the
--- whitespace, and it just calls getToken again.
+-- | skipWhitespace expects to recieve parameters that have already consumed the
+-- whitespace or control character, and calls getToken with the
+-- modified source and column/line numbers.
 skipWhitespace :: (String, String, Int, Int) -> (String, String, Int, Int)
-skipWhitespace (source, lexeme, columnNumber, lineNumber) =
-    getToken (source, lexeme, columnNumber, lineNumber)
+skipWhitespace (source, lexeme, columnNumber, lineNumber)
+    | isControl nextChar
+        = getToken (source, lexeme, 0, lineNumber + 1)
+    | nextChar == ' '
+        = getToken (source, lexeme, columnNumber + 1, lineNumber)
+  where
+    nextChar = head source  -- get the next character
+
+-- | Gets the lexeme currently being passed around and returns it.
+getLexeme :: (String, String, Int, Int) -> String
+getLexeme (source, lexeme, columnNumber, lineNumber) = lexeme
+
+-- | Gets the line number currently being passed around and returns it.
+getLineNumber :: (String, String, Int, Int) -> Int
+getLineNumber (source, lexeme, columnNumber, lineNumber) = lineNumber
+
+-- | Gets the column number currently being passed around and returns it.
+getColumnNumber :: (String, String, Int, Int) -> Int
+getColumnNumber (source, lexeme, columnNumber, lineNumber) = columnNumber
--- mp.hs
+++ mp.hs
@@ -1,19 +1,21 @@
---Authored by: Tyler J. Huffman
---Adapted from http://learnyouahaskell.com/input-and-output
+-- Authored by: Tyler J. Huffman, Ryan "Murph" Murphy
+-- Date: 1/24/2013
+-- Adapted from http://learnyouahaskell.com/input-and-output
 --
---This file takes in one argument and throws away the rest.
---It's also extensible enough to allow for more arguments by
---modifying the pattern-matching at the start of the "do"
---function
+-- This file takes in one argument and throws away the rest.
+-- It's also extensible enough to allow for more arguments by
+-- modifying the pattern-matching at the start of the "do"
+-- function
 --
---Also, this is my first attempt, so I'll likely be modifying
---this as time goes on.
+-- The toTry function then reads the file into a string, and calls
+-- getToken with the default values.
 
 import System.Environment
 import System.IO
 import System.Directory
 import Prelude hiding (catch)
 import Control.Exception
+
 import Scanner
 
 main = toTry `catch` handler
@@ -21,13 +23,13 @@ main = toTry `catch` handler
 toTry :: IO ()
 toTry = do (filename:_) <- getArgs
             if (dropWhile (/= '.') filename) == ".mp"
-            then getNextToken (source, lexeme, columnNumber, lineNumber)
-                where
-                    source = readFile filename
+                then getNextToken (source, lexeme, columnNumber, lineNumber)
+                  where
+                    source = read filename
                     lexeme = ""
                     columnNumber = 0
                     lineNumber = 0
-            else putStrLn "Please insert a valid file"
+                else putStrLn "Please insert a valid file."
 
-handler :: IOError -> IO ()  
-handler e = putStrLn "Please insert a valid file"
+handler :: IOError -> IO ()
+handler e = putStrLn "Please insert a valid file."
