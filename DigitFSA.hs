9d2ebd069db5e794cf2251a4c6eff7ac9d9406e3
Tyler Huffman <tyler.huffman@tylerh.org>
2013-01-24 08:08:44 -0700

    Did simple modifications to the tree, unfinished testing

--- DigitFSA.hs
+++ DigitFSA.hs
@@ -1,16 +1,25 @@
 --Authored by Tyler J. Huffman
 --
 --This is the list of functions used in the microPascal compiler
-import Data.list
+module DigitFSA where
+import Data.List
+
 
 digitFSA :: (String, String, Int, Int) -> (String, String, Int, Int)
 digitFSA (src, lexeme, column_number, line_number)
-    | stringHead == [0..9]    = digitFSA (tail src, lexeme : stringHead, column_number + 1, line_number)
-    | stringHead == ('e'|'E') = digitFSAforE (tail src, lexeme : stringHead, column_number + 1, line_number)
-    | stringHead == '.'       = digitFSAforPeriod (tail src, lexeme : stringHead, column_number + 1, line_number)
-    | otherwise               = (src, lexeme, column_number, line_number)
-    where read stringHead = $ read . head src
+    | stringHead == '0'                  
+        = digitFSA (tail src, lexeme, column_number + 1, line_number)
+    | stringHead == 'e' || stringHead == 'E' 
+        = digitFSAforE (tail src, lexeme, column_number + 1, line_number)
+    | stringHead == '.'                     
+        = digitFSAforPeriod (tail src, lexeme, column_number + 1, line_number)
+    | otherwise                             
+        = (src, lexeme, column_number, line_number)
+    where stringHead = src !! 0
 
---digitFSAforE :: (String, String, Int, Int) -> (String, String, Int, Int)
---digitFSAforE (src, lexeme, column_number, line_number)
+digitFSAforE :: (String, String, Int, Int) -> (String, String, Int, Int)
+digitFSAforE (src, lexeme, column_number, line_number) = (src, lexeme, column_number, line_number)
 --    | stringHead
+digitFSAforPeriod :: (String, String, Int, Int) -> (String, String, Int, Int)
+digitFSAforPeriod (src, lexeme, column_number, line_number) = (src, lexeme, column_number, line_number)
+
