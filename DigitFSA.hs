d05e7d6fa2314674eb59a433ecd4ba45ea332308
Huffman <huffmaty@bznlinux017.spray.com>
2013-01-24 10:19:09 -0700

    More minor changes. Issues with types.

--- DigitFSA.hs
+++ DigitFSA.hs
@@ -3,23 +3,25 @@
 --This is the list of functions used in the microPascal compiler
 module DigitFSA where
 import Data.List
+import Data.Char
 
 
 digitFSA :: (String, String, Int, Int) -> (String, String, Int, Int)
 digitFSA (src, lexeme, column_number, line_number)
-    | stringHead == '0'                  
-        = digitFSA (tail src, lexeme, column_number + 1, line_number)
+    | isDigit stringHead
+        = digitFSA (tail src, (show stringHead) : lexeme, column_number + 1, line_number)
     | stringHead == 'e' || stringHead == 'E' 
-        = digitFSAforE (tail src, lexeme, column_number + 1, line_number)
+        = digitFSAforE (tail src, (show stringHead) : lexeme, column_number + 1, line_number)
     | stringHead == '.'                     
-        = digitFSAforPeriod (tail src, lexeme, column_number + 1, line_number)
+        = digitFSAforPeriod (tail src, (show stringHead) : lexeme, column_number + 1, line_number)
     | otherwise                             
         = (src, lexeme, column_number, line_number)
-    where stringHead = src !! 0
+    where stringHead = head src
 
 digitFSAforE :: (String, String, Int, Int) -> (String, String, Int, Int)
 digitFSAforE (src, lexeme, column_number, line_number) = (src, lexeme, column_number, line_number)
 --    | stringHead
+
 digitFSAforPeriod :: (String, String, Int, Int) -> (String, String, Int, Int)
 digitFSAforPeriod (src, lexeme, column_number, line_number) = (src, lexeme, column_number, line_number)
 
