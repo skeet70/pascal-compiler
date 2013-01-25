38a36e8956c4ad16f92f07296d47e0e9df9e7b39
Tyler Huffman <tyler.huffman@tylerh.org>
2013-01-24 21:04:01 -0700

    Deleted test garbage

--- test.hs
+++
@@ -1,8 +0,0 @@
-import System.Environment  
-import System.IO  
-import System.Directory  
-import DigitFSA
-  
-main = do 
-    let tuple = ("0000101001", [], 0, 0)
-        in putStrLn $ digitFSA tuple
