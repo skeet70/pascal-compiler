bf1d2254bf4fa040fb1ead5182abd12988ef50c5
iduhetonas <tyler.huffman@tylerh.org>
2013-01-29 17:41:37 -0700

    Finished the TokenTable.hs file and helper functions. Closes #23

--- TokenTable.hs
+++ TokenTable.hs
@@ -1,3 +1,4 @@
+module TokenTable where
 --Authored by: Tyler J. Huffman
 data ReservedWords = MP_AND | MP_BEGIN | MP_DIV | MP_DO | MP_DOWNTO | MP_ELSE
     | MP_END | MP_FIXED | MP_FLOAT | MP_FOR | MP_FUNCTION | MP_IF
@@ -8,6 +9,7 @@ data ReservedWords = MP_AND | MP_BEGIN | MP_DIV | MP_DO | MP_DOWNTO | MP_ELSE
 
 data IdentifiersAndLiterals = MP_IDENTIFIER | MP_INTEGER_LIT | MP_FIXED_LIT
     | MP_FLOAT_LIT | MP_STRING_LIT
+    deriving (Eq, Show, Read)
 
 data ErrorCodes = MP_RUN_COMMENT | MP_RUN_STRING | MP_ERROR
     deriving (Eq, Show, Read)
@@ -15,9 +17,25 @@ data ErrorCodes = MP_RUN_COMMENT | MP_RUN_STRING | MP_ERROR
 data Symbols = MP_PERIOD | MP_COMMA | MP_SCOLON | MP_LPAREN | MP_EQUAL
     | MP_GTHAN | MP_LTHAN | MP_LEQUAL | MP_NEQUAL | MP_ASSIGN | MP_PLUS
     | MP_MINUS | MP_TIMES | MP_COLON
+    deriving (Eq, Show, Read)
 
 data EndOfFile = MP_EOF
-
-data Token a = ReservedWords a | IdentifiersAndLiterals a | ErrorCodes a
-    | Symbols a | EndOfFile a
     deriving (Eq, Show, Read)
+
+data Token = ReservedWords ReservedWords | IdentifiersAndLiterals IdentifiersAndLiterals | ErrorCodes ErrorCodes 
+    | Symbols Symbols | EndOfFile EndOfFile
+    deriving (Eq, Show, Read)
+
+unwrapToken :: Token -> String
+unwrapToken (ReservedWords x)           = show x
+unwrapToken (IdentifiersAndLiterals x)  = show x
+unwrapToken (ErrorCodes x)              = show x
+unwrapToken (Symbols x)                 = show x
+unwrapToken (EndOfFile x)               = show x
+
+getTokenType :: Token -> String
+getTokenType (ReservedWords x)          = "ReservedWords"
+getTokenType (IdentifiersAndLiterals x) = "IdentifiersAndLiterals"
+getTokenType (ErrorCodes x)             = "ErrorCodes"
+getTokenType (Symbols x)                = "Symbols"
+getTokenType (EndOfFile x)              = "EndOfFile"
