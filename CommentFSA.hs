b5b4a406f1e8a5cf087e41d91599ebbf65267ec8
iduhetonas <tyler.huffman@tylerh.org>
2013-01-30 10:37:54 -0700

    Created a 'Maybe' non-concrete type to accept 'Just Char' instead of 'Char'

--- CommentFSA.hs
+++ CommentFSA.hs
@@ -1,37 +1,38 @@
+import TokenTable
 --Author Tyler J. Huffman
 --Top-level state machine to remove comments from a source string
 commentFSA :: (String, String, Int, Int) -> (String, String, Token, Int, Int)
 commentFSA (src, lexeme, column_number, line_number) 
-    | stringHead == '{'
-        = bracketFSA (tail src, lexeme, column_number + 1, line_number)
-    | stringHead == '(' && stringNext == '*'
-        = parenFSA (tail . tail src, lexeme, column_number + 2, line_number)
+    | stringHead == Just '{'
+        = bracketFSA (tail src, lexeme, column_number, line_number)
+    | stringHead == Just '(' && stringNext == Just '*'
+        = parenFSA (tail (tail src), lexeme, column_number, line_number)
     | otherwise
         = (src, lexeme, ErrorCodes MP_ERROR, column_number, line_number)
-    where stringHead = if src == [] then ' ' else head src
+    where stringHead = if src == [] then Nothing else Just (head src)
           stringNext
-              | stringHead /= ' ' && tail src /= [] = src !! 1
-              | otherwise = 'x'
+              | stringHead /= Nothing && tail src /= [] = Just (src !! 1)
+              | otherwise = Nothing
 
 bracketFSA :: (String, String, Int, Int) -> (String, String, Token, Int, Int)
 bracketFSA (src, lexeme, column_number, line_number)
-    | stringHead == '}'
-        = (tail src, lexeme, IdentifiersAndLiterals MP_STRING_LIT, column_number + 1, line_number)
-    | stringHead == ' '
+    | stringHead == Just '}'
+        = (tail src, lexeme, IdentifiersAndLiterals MP_STRING_LIT, column_number, line_number)
+    | stringHead == Nothing
         = (src, lexeme, ErrorCodes MP_RUN_STRING, column_number, line_number)
     | otherwise
-        = bracketFSA(tail src, lexeme, column_number + 1, line_number)
-    where stringHead = if src == [] then ' ' else head src
+        = bracketFSA(tail src, lexeme, column_number, line_number)
+    where stringHead = if src == [] then Nothing else Just (head src)
 
 parenFSA :: (String, String, Int, Int) -> (String, String, Token, Int, Int)
 parenFSA (src, lexeme, column_number, line_number)
-    | stringHead == '*' && stringNext == ')'
-        = (tail . tail src, lexeme, IdentifiersAndLiterals MP_STRING_LIT, column_number + 2, line_number)
-    | stringHead == ' '
+    | stringHead == Just '*' && stringNext == Just ')'
+        = (tail (tail src), lexeme, IdentifiersAndLiterals MP_STRING_LIT, column_number, line_number)
+    | stringHead == Nothing
         = (src, lexeme, ErrorCodes MP_RUN_STRING, column_number, line_number)
     | otherwise
-        = parenFSA (tail src, lexeme, column_number + 1, line_number)
-    where strigHead = if src == [] then ' ' else head src
+        = parenFSA (tail src, lexeme, column_number, line_number)
+    where stringHead = if src == [] then Nothing else Just (head src)
           stringNext
-              | stringHead /= ' ' && tail src /= [] = src !! 1
-              | otherwise = 'x'
+              | stringHead /= Nothing && tail src /= [] = Just (src !! 1)
+              | otherwise = Nothing
