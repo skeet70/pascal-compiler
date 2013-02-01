6e94d8b13b151789bdf9104cc4364e5182c48556
iduhetonas <tyler.huffman@tylerh.org>
2013-01-31 23:22:27 -0700

    Finished modifying (untested) the LetterFSA.hs file

--- letterFSA.hs
+++ letterFSA.hs
@@ -1,4 +1,4 @@
--- Authored by: James Sonntag, Ryan "Murph" Murphy, and Tyler Huff-man.
+-- Authored by: James Sonntag, Ryan "Murph" Murphy, and Tyler Huffman.
 -- Creation Date: 1/24/2013
 -- Most Recent Update: 1/30/2013
 -- This file contains the State-Machines that relate to Identifiers, Strings, and Symbols.
@@ -14,8 +14,8 @@ import TokenTable
 -- send the proper identifier token.
 identifierFSA :: (String, String, Int, Int) -> (String, String, Token, Int, Int)
 identifierFSA (source, lexeme, colNum, lineNum)
-  | isAlphaNum (fromJust stringHead)                                   = identifierFSA(tail source, lexeme ++ (fromJust stringHead), colNum+1, lineNum)
-  | stringHead == Just '_' && isAlphaNum (fromJust stringNext)         = identifierFSA(tail source, lexeme ++ (fromJust stringHead), colNum+1, lineNum)
+  | isAlphaNum (fromJust stringHead)                                   = identifierFSA(tail source, lexeme ++ (charToString (fromJust stringHead)), colNum+1, lineNum)
+  | stringHead == Just '_' && isAlphaNum (fromJust stringNext)         = identifierFSA(tail source, lexeme ++ (charToString (fromJust stringHead)), colNum+1, lineNum)
   | null source                                                         = (source, lexeme, IdentifiersAndLiterals MP_IDENTIFIER, colNum, lineNum)
   | stringHead == Just '_' && not (isAlphaNum (fromJust stringNext))   = (source, lexeme, IdentifiersAndLiterals MP_IDENTIFIER, colNum, lineNum)
   | otherwise                                                           = (source, lexeme, IdentifiersAndLiterals MP_IDENTIFIER, colNum, lineNum)
@@ -31,9 +31,9 @@ identifierFSA (source, lexeme, colNum, lineNum)
 stringFSA :: (String, String, Int, Int) -> (String, String, Token, Int, Int)
 stringFSA (source, lexeme, colNum, lineNum)
   | stringHead == Just '\'' && null lexeme                            = stringFSA(tail source, lexeme, colNum+1, lineNum)
-  | stringHead == Just '\'' && stringNext == Just '\''                = stringFSA(tail (tail source), lexeme ++ (fromJust stringHead), colNum+2, lineNum)
+  | stringHead == Just '\'' && stringNext == Just '\''                = stringFSA(tail (tail source), lexeme ++ (charToString (fromJust stringHead)), colNum+2, lineNum)
   | stringHead == Just '\\' && stringNext == Just 'n'                 = stringFSA(tail (tail source), lexeme ++ "\n", colNum+2, lineNum)
-  | isAscii (fromJust stringHead)                                    = stringFSA(tail source, lexeme ++ (fromJust stringHead), colNum+1, lineNum)
+  | isAscii (fromJust stringHead)                                    = stringFSA(tail source, lexeme ++ (charToString (fromJust stringHead)), colNum+1, lineNum)
   | null source                                                           = (source, lexeme, IdentifiersAndLiterals MP_STRING_LIT, colNum, lineNum)
   | stringHead == Just '\'' && stringNext == Just '\'' && null lexeme     = (tail (tail source), lexeme, IdentifiersAndLiterals MP_STRING_LIT, colNum+2, lineNum)
   | stringHead == Just '\''                                               = (tail source, lexeme, IdentifiersAndLiterals MP_STRING_LIT, colNum+1, lineNum)
@@ -49,8 +49,8 @@ stringFSA (source, lexeme, colNum, lineNum)
 -- send the proper tokens.
 gthanFSA :: (String, String, Int, Int) -> (String, String, Token, Int, Int)
 gthanFSA (source, lexeme, colNum, lineNum) 
-  | stringHead == Just '>' && stringNext == Just '='        = (tail (tail source), lexeme ++ (fromJust stringHead) ++ (fromJust stringNext), Symbols MP_GEQUAL, colNum+2, lineNum)
-  | stringHead == Just '>'                                  = (tail source, lexeme ++ (fromJust stringHead), Symbols MP_GTHAN, colNum+1, lineNum)
+  | stringHead == Just '>' && stringNext == Just '='        = (tail (tail source), lexeme ++ (charToString (fromJust stringHead)) ++ (charToString (fromJust stringNext)), Symbols MP_GEQUAL, colNum+2, lineNum)
+  | stringHead == Just '>'                                  = (tail source, lexeme ++ (charToString (fromJust stringHead)), Symbols MP_GTHAN, colNum+1, lineNum)
   where stringHead = if source == [] then Nothing else Just (head source)
         stringNext
               | stringHead /= Nothing && tail source /= [] = Just (source !! 1)
@@ -60,9 +60,9 @@ gthanFSA (source, lexeme, colNum, lineNum)
 -- send the proper tokens.
 lthanFSA :: (String, String, Int, Int) -> (String, String, Token, Int, Int)
 lthanFSA (source, lexeme, colNum, lineNum)
-  | stringHead == Just '<' && stringNext == Just '='        = (tail (tail source), lexeme ++ (fromJust stringHead) ++ (fromJust stringNext), Symbols MP_LEQUAL, colNum+2, lineNum)
-  | stringHead == Just '<' && stringNext == Just '>'        = (tail (tail source), lexeme ++ (fromJust stringHead) ++ (fromJust stringNext), Symbols MP_NEQUAL, colNum+2, lineNum)
-  | stringHead == Just '<'                                  = (tail source, lexeme ++ (fromJust stringHead), Symbols MP_LTHAN, colNum+1, lineNum)
+  | stringHead == Just '<' && stringNext == Just '='        = (tail (tail source), lexeme ++ (charToString (fromJust stringHead)) ++ (charToString (fromJust stringNext)), Symbols MP_LEQUAL, colNum+2, lineNum)
+  | stringHead == Just '<' && stringNext == Just '>'        = (tail (tail source), lexeme ++ (charToString (fromJust stringHead)) ++ (charToString (fromJust stringNext)), Symbols MP_NEQUAL, colNum+2, lineNum)
+  | stringHead == Just '<'                                  = (tail source, lexeme ++ (charToString (fromJust stringHead)), Symbols MP_LTHAN, colNum+1, lineNum)
   where stringHead = if source == [] then Nothing else Just (head source)
         stringNext
               | stringHead /= Nothing && tail source /= [] = Just (source !! 1)
@@ -72,9 +72,16 @@ lthanFSA (source, lexeme, colNum, lineNum)
 -- send the proper tokens.
 colonFSA :: (String, String, Int, Int) -> (String, String, Token, Int, Int)
 colonFSA (source, lexeme, colNum, lineNum)
-  | stringHead == Just ':' && stringNext == Just '=' = (tail (tail source), lexeme ++ (fromJust stringHead) ++ (fromJust stringNext), Symbols MP_ASSIGN, colNum+2, lineNum)
-  | stringHead == Just ':'                                = (tail source, lexeme ++ (fromJust stringHead), Symbols MP_COLON, colNum+1, lineNum)
+  | stringHead == Just ':' && stringNext == Just '=' = (tail (tail source), lexeme ++ (charToString (fromJust stringHead)) ++ (charToString (fromJust stringNext)), Symbols MP_ASSIGN, colNum+2, lineNum)
+  | stringHead == Just ':'                                = (tail source, lexeme ++ (charToString (fromJust stringHead)), Symbols MP_COLON, colNum+1, lineNum)
   where stringHead = if source == [] then Nothing else Just (head source)
         stringNext
               | stringHead /= Nothing && tail source /= [] = Just (source !! 1)
               | otherwise = Nothing
+
+--Helper function that turns a Char into a String
+--
+--Parameters: Char
+--Returns: String
+charToString :: Char -> String
+charToString c = [c]
