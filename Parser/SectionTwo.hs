7b53b292e5c56d25815aa71faa43d6e4d396cfce
Gabrinthei <james.s.sunday@gmail.com>
2013-02-15 10:45:01 -0700

    Added the failure recurse back and syntaxError

--- Parser/SectionTwo.hs
+++ Parser/SectionTwo.hs
@@ -24,6 +24,8 @@ import Scanner.TokenType
 --          ⟶ ProcedureStatement
 statement :: ParsingData -> ParsingData
 statement parsingData
+    | hasFailed parsingData == True
+        = parsingData
     | (lookAheadToken parsingData) == lambda
         = emptyStatement parsingData
     | unwrapToken ((lookAheadToken parsingData)) ==  "MP_BEGIN"
@@ -45,189 +47,234 @@ statement parsingData
     | unwrapToken (lookAheadToken parsingData) == "MP_IDENTIFIER"
         = procedureStatement parsingData
     | otherwise
-        = -- handle error
+        = syntaxError parsingData
 
 emptyStatement :: ParsingData -> ParsingData
 emptyStatement parsingData
+    | hasFailed parsingData == True
+        = parsingData
     | (lookAheadToken parsingData) == lambda
-        = --handle lambda
+        = parsingData
+    | otherwise
+        syntaxError parsingData
 
 --ReadStatement ⟶ "read" "(" ReadParameter ReadParameterTail ")"
 readStatement :: ParsingData -> ParsingData
 readStatement parsingData
+    | hasFailed parsingData == True
+        = parsingData
     | unwrapToken (lookAheadToken parsingData) ==  "MP_READ"
         = terminal (readParameterTail (readParameter (terminal (terminal parsingData))))
     | otherwise
-        = --handle error
+        = syntaxError parsingData
 
 --ReadParameterTail ⟶ "," ReadParameter ReadParameterTail
 --                  ⟶ ε
 readParameterTail :: ParsingData -> ParsingData
 readParameterTail parsingData
+    | hasFailed parsingData == True
+        = parsingData
     | unwrapToken (lookAheadToken parsingData) == "MP_COMMA"
         = readParameterTail (readParameter (terminal parsingData))
     | (lookAheadToken parsingData) == lambda
-        = --handle lambda
+        = parsingData
     | otherwise
-        = --handle error
+        = syntaxError parsingData
 
 --ReadParameter ⟶ VariableIdentifier
 readParameter :: ParsingData -> ParsingData
 readParameter parsingData
+    | hasFailed parsingData == True
+        = parsingData
     | unwrapToken (lookAheadToken parsingData) == "MP_IDENTIFIER"
         = variableIdentifier parsingData
     | otherwise
-        = --handle error
+        = syntaxError parsingData
 
 --WriteStatement ⟶ "write" "(" WriteParameter WriteParameterTail ")"
 writeStatement :: ParsingData -> ParsingData
 writeStatement parsingData
+    | hasFailed parsingData == True
+        = parsingData
     | unwrapToken (lookAheadToken parsingData) == "MP_WRITE"
         = terminal (writeParameterTail (writeParameter (terminal (terminal parsingData))))
     | otherwise
-        = --handle error
+        = syntaxError parsingData
 
 --WriteParameterTail  ⟶ "," WriteParameter
 --                    ⟶ ε
 writeParameterTail :: ParsingData -> ParsingData
 writeParameterTail parsingData
+    | hasFailed parsingData == True
+        = parsingData
     | unwrapToken (lookAheadToken parsingData) == "MP_COMMA"
         = writeParameter (terminal parsingData)
     | (lookAheadToken parsingData) == lambda
-        = --handle lambda
+        = parsingData
     | otherwise
-        = --handle error
+        = syntaxError parsingData
 
 --WriteParameter ⟶ OrdinalExpression
 writeParameter :: ParsingData -> ParsingData
 writeParameter parsingData
+    | hasFailed parsingData == True
+        = parsingData
     | any (== unwrapToken (lookAheadToken parsingData)) ["MP_PLUS", "MP_MINUS"] || (lookAheadToken parsingData) == lambda
         = ordinalExpression parsingData
     | otherwise
-        = --handle error
+        = syntaxError parsingData
 
 --AssignmentStatement ⟶ VariableIdentifier ":=" Expression
 --                    ⟶ FunctionIdentifier ":=" Expression 
 assignmentStatement :: ParsingData -> ParsingData
 assignmentStatement parsingData
+    | hasFailed parsingData == True
+        = parsingData
     | unwrapToken (lookAheadToken parsingData) == "MP_IDENTIFIER"
         = expression (terminal (variableIdentifier parsingData))
     | getTokenType (lookAheadToken parsingData) == "ReservedWord"
         = expression (terminal (functionIdentifier parsingData))  --Just for now, need to get clarification from Rocky
     | otherwise
-        = --handle error
+        = syntaxError parsingData
 
 --IfStatement ⟶ "if" BooleanExpression "then" Statement OptionalElsePart
 ifStatement :: ParsingData -> ParsingData
 ifStatement parsingData
+    | hasFailed parsingData == True
+        = parsingData
     | unwrapToken (lookAheadToken parsingData) == "MP_IF"
         = optionalElsePart (statment (terminal (booleanExpression (terminal parsingData))))
     | otherwise
-        = --handle error
+        = syntaxError parsingData
 
 --OptionalElsePart ⟶ "else" Statement
 --                 ⟶ ε  
 optionalElsePart :: ParsingData -> ParsingData
 optionalElsePart parsingData
+    | hasFailed parsingData == True
+        = parsingData
     | unwrapToken (lookAheadToken parsingData) == "MP_ELSE"
         = statement (terminal parsingData)
     | (lookAheadToken parsingData) == lambda
-        = --handle lambda
+        = parsingData
     |otherwise
-        = --handle error
+        = syntaxError parsingData
 
 --RepeatStatement ⟶ "repeat" StatementSequence "until" BooleanExpression  
 repeatStatement :: ParsingData -> ParsingData
 repeatStatement parsingData
+    | hasFailed parsingData == True
+        = parsingData
     | unwrapToken (lookAheadToken parsingData) == "MP_REPEAT"
         = booleanExpression (terminal (statementSequence (terminal parsingData)))
     | otherwise
-        = --handle error
+        = syntaxError parsingData
 
 --WhileStatement ⟶ "while" BooleanExpression "do" Statement  
 whileStatement :: ParsingData -> ParsingData
 whileStatement parsingData
+    | hasFailed parsingData == True
+        = parsingData
     | unwrapToken (lookAheadToken parsingData) == "MP_WHILE"
         = statement (terminal (booleanExpression (terminal parsingData)))
     | otherwise
-        = --handle error
+        = syntaxError parsingData
 
 --ForStatement ⟶ "for" ControlVariable ":=" InitialValue StepValue FinalValue "do" Statement
 forStatement :: ParsingData -> ParsingData
 forStatement parsingData
+    | hasFailed parsingData == True
+        = parsingData
     | unwrapToken (lookAheadToken parsingData) == "MP_FOR"
         = statement (terminal (finalValue (stepValue (initialValue (terminal (controlVariable (terminal parsingData)))))))
     | otherwise
-        = --handle error
+        = syntaxError parsingData
 
 --ControlVariable ⟶ VariableIdentifier
 controlVariable :: ParsingData -> ParsingData
 controlVariable parsingData
+    | hasFailed parsingData == True
+        = parsingData
     | unwrapToken (lookAheadToken parsingData) == "MP_IDENTIFIER"
         = variableIdentifier parsingData
     | otherwise
-        = --handle error
+        = syntaxError parsingData
 
 --InitialValue ⟶ OrdinalExpression
 initialValue :: ParsingData -> ParsingData
 initialValue parsingData
+    | hasFailed parsingData == True
+        = parsingData
     | any (== unwrapToken (lookAheadToken parsingData)) ["MP_PLUS", "MP_MINUS"] || (lookAheadToken parsingData) == lambda
         = ordinalExpression parsingData
     | otherwise
-        = --handle error
+        = syntaxError parsingData
 
 --StepValue ⟶ "to"
 --          ⟶ "downto"
 stepValue :: ParsingData -> ParsingData
+stopValue parsingData
+    | hasFailed parsingData == True
+        = parsingData
     | unwrapToken (lookAheadToken parsingData) == "MP_TO"
         = terminal parsingData
     | unwrapToken (lookAheadToken parsingData) == "MP_DOWNTO"
         = terminal parsingData
     | otherwise
-        = --handle error
+        = syntaxError parsingData
 
 --FinalValue ⟶ OrdinalExpression
 finalValue :: ParsingData -> ParsingData
 finalValue parsingData
+    | hasFailed parsingData == True
+        = parsingData
     | any (== unwrapToken ((lookAheadToken parsingData))) ["MP_PLUS", "MP_MINUS"] || (lookAheadToken parsingData) == lambda
         = ordinalExpression parsingData
     | otherwise
-        = --handle error
+        = syntaxError parsingData
 
 --ProcedureStatement ⟶ ProcedureIdentifier OptionalActualParameterList
 procedureStatement :: ParsingData -> ParsingData
 procedureStatement parsingData
+    | hasFailed parsingData == True
+        = parsingData
     | unwrapToken (lookAheadToken parsingData) == "MP_IDENTIFIER"
         = optionalActualParameterList (procedureIdentifier parsingData)
     | otherwise
-        = --handle error
+        = syntaxError parsingData
 
 --OptionalActualParameterList ⟶ "(" ActualParameter ActualParameterTail ")"
 --                            ⟶ ε
 optionalActualParameterList :: ParsingData -> ParsingData
 optionalActualParameterList parsingData
+    | hasFailed parsingData == True
+        = parsingData
     | unwrapToken (lookAheadToken parsingData) == "MP_LPAREN"
         = terminal (actualParameterTail (actualParameter (terminal parsingData)))
     | (lookAheadToken parsingData) == lambda
-        = --handle lambda
+        = parsingData
     | otherwise
-        = --handle error
+        = syntaxError parsingData
 
 --ActualParameterTail ⟶ "," ActualParameter ActualParameterTail
 --                    ⟶ ε
 actualParameterTail :: ParsingData -> ParsingData
 actualParameterTail parsingData
+    | hasFailed parsingData == True
+        = parsingData
     | unwrapToken (lookAheadToken parsingData) == "MP_COMMA"
         = actualParameterTail (actualParameter (terminal parsingData))
     | (lookAheadToken parsingData) == lambda
-        = --handle lambda
+        = parsingData
     | otherwise
-        = --handle error
+        = syntaxError parsingData
 
 --ActualParameter ⟶ OrdinalExpression
 actualParameter :: ParsingData -> ParsingData
 actualParameter parsingData
+    | hasFailed parsingData == True
+        = parsingData
     | any (== unwrapToken ((lookAheadToken parsingData))) ["MP_PLUS", "MP_MINUS"] || (lookAheadToken parsingData) == lambda
         = ordinalExpression parsingData
     | otherwise
-        = --handle error
+        = syntaxError parsingData
