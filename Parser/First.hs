43fc8e54889e569eb301d2792d4db999ed689915
iduhetonas <tyler.huffman@tylerh.org>
2013-02-15 13:32:17 -0700

    Finished First (untested) section of parser functions

--- Parser/First.hs
+++ Parser/First.hs
@@ -1,3 +1,7 @@
+--First section of Parsing Functions
+--
+--Authored by: Tyler J. Huffman
+
 systemGoal :: ParsingData -> ParsingData
 systemGoal parsingData
     | unwrapToken (lookAheadToken parsingData) == "MP_EOF"
@@ -113,4 +117,54 @@ formalParameterSectionTail parsingData
     | otherwise
         = syntaxError parsingData
 
+formalParameterSection :: ParsingData -> ParsingData
+formalParameterSection parsingData
+    | unwrapToken (lookAheadToken parsingData) == "MP_IDENTIFIER"  
+        = valueParameterSection parsingData
+    | unwrapToken (lookAheadToken parsingData) == "MP_VAR"  
+        = variableParameterSection
+    | otherwise
+        = syntaxError parsingData
 
+valueParameterSection :: ParsingData -> ParsingData
+valueParameterSection parsingData
+    | unwrapToken (lookAheadToken parsingData) == "MP_IDENTIFIER"   
+        = typeParser ( terminal ( identifierList parsingData))
+    | otherwise
+        = syntaxError parsingData
+
+variableParameterSection :: ParsingData -> ParsingData
+variableParameterSection parsingData
+    | unwrapToken (lookAheadToken parsingData) == "MP_VAR"   
+        = typeParser ( terminal ( identifierList ( terminal parsingData)))
+    | otherwise
+        = syntaxError parsingData
+
+statementPart :: ParsingData -> ParsingData
+statementPart parsingData
+    | unwrapToken (lookAheadToken parsingData) == "MP_BEGIN"
+        = compoundStatement parsingData
+    | otherwise
+        = syntaxError parsingData
+
+compoundStatement :: ParsingData -> ParsingData
+compoundStatement parsingData
+    | unwrapToken (lookAheadToken parsingData) == "MP_BEGIN"
+        = terminal ( statementSequence ( terminal parsingData))
+    | otherwise
+        = syntaxError parsingData
+
+statementSequence :: ParsingData -> ParsingData
+statementSequence parsingData
+    | any (unwrapToken (lookAheadToken parsingData) ==) ["MP_BEGIN", "MP_READ", "MP_WRITE", "MP_IDENTIFIER", "MP_IF", "MP_WHILE", "MP_REPEAT", "MP_FOR"]
+        = statementTail ( statement ( parsingData))
+    | otherwise
+        = syntaxError parsingData
+
+statementTail :: ParsingData -> ParsingData
+    | unwrapToken (lookAheadToken parsingData) == "MP_SCOLON"
+        = statementTail ( statement ( terminal parsingData))
+    | unwrapToken (lookAheadToken parsingData) == lambda
+        = parsingData
+    | otherwise
+        = syntaxError parsingData
