a531b225f740d3252c0e3a9ba03e3b82150f8ee2
Murph Murphy <omurcada@gmail.com>
2013-04-23 21:40:05 -0600

    * Quick comment in case anyone is pulling this.

--- IntermediateCode/IRHelpers.hs
+++ IntermediateCode/IRHelpers.hs
@@ -6,6 +6,9 @@
 
 module IntermediateCode.IRHelpers where
 
+-- Datatype to contain IR information. Contains a counter to be incremented to
+-- keep track of label numbers, as well as a flag to be switched if the next
+-- operation needs to use float/fixed numbers instead of integers.
 data SemanticRecord = SemanticRecord { labelNumber :: Int
                                      , isFloat :: Bool
                                     } deriving (Show)
