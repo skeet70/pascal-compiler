2f9c2c6115efc7809051f522c72ed3a82c71d7c1
Murph Murphy <omurcada@gmail.com>
2013-04-29 11:33:57 -0600

    * Adding support for class tests.

--- test.sh
+++ test.sh
@@ -5,3 +5,10 @@ do
    echo $file >> test_output.pas
    ./mp $file >> test_output.pas
 done
+
+for file in Tests/TestsFromPersons/tests/*.mp
+do
+   echo "" >> test_output.pas
+   echo $file >> test_output.pas
+   ./mp $file >> test_output.pas
+done
